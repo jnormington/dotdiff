64d3fc9575cabd65d654788264e569e844fd36d4
Jon Normington <jnormington@users.noreply.github.com>
2016-03-25 20:31:05 +0000

    Update other specs to use the global MocksHelper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,8 @@ require 'dotdiff'
 require_relative './helper/mocks_helper'
 
 RSpec.configure do |config|
-  config.include MocksHelper
+  include MocksHelper
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
