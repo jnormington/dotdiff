602aa5a7e5730accccb000c3c0e56921975c77d4
Jon Normington <jnormington@users.noreply.github.com>
2021-03-19 21:10:11 +0000

    feat: Simplify comparer class
    So that now it will call out to the relevant
    comparible classes for the page and element comparison

--- lib/dotdiff/comparer.rb
+++ lib/dotdiff/comparer.rb
@@ -12,49 +12,12 @@ module DotDiff
 
     def result
       if element.is_a?(Capybara::Session)
-        compare_page
+        DotDiff::Comparible::PageComparer.run(snapshot, nil)
       elsif element.is_a?(Capybara::Node::Base)
-        compare_element
+        DotDiff::Comparible::ElementComparer.run(snapshot, ElementMeta.new(page, element))
       else
         raise ArgumentError, "Unknown element class received: #{element.class.name}"
       end
-    end
-
-    private
-
-    def compare_element(element_meta = ElementMeta.new(page, element))
-      snapshot.capture_from_browser(DotDiff.hide_elements_on_non_full_screen_screenshot)
-      snapshot.crop_and_resave(element_meta)
-
-      if !File.exist?(snapshot.basefile)
-        snapshot.resave_cropped_file
-        [true, snapshot.basefile]
-      else
-        compare(snapshot.cropped_file)
-      end
-    end
-
-    def compare_page
-      snapshot.capture_from_browser(true)
-
-      if !File.exist?(snapshot.basefile)
-        snapshot.resave_fullscreen_file
-        [true, snapshot.basefile]
-      else
-        compare(snapshot.fullscreen_file)
-      end
-    end
-
-    def compare(compare_to_image)
-      result = CommandWrapper.new
-      result.run(snapshot.basefile, compare_to_image, snapshot.diff_file)
-
-      if result.failed? && DotDiff.failure_image_path
-        FileUtils.mkdir_p(snapshot.failure_path)
-        FileUtils.mv(compare_to_image, snapshot.new_file, force: true)
-      end
-
-      [result.passed?, result.message]
     end
   end
 end
